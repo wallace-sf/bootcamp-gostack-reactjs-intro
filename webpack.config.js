8359014f35b8b68b9a913326fa3a9018e83d1147
unknown <Wallace@WALLACE-PC.marestdt.com>
2020-02-11 22:22:47 -0300

    chore: Add Repository

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const { resolve } = require('path');
+
+module.exports = {
+  entry: resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+}
