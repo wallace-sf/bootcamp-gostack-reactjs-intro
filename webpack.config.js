5ef364074f43f85688834a75c0aa2f8d83f77b23
unknown <Wallace@WALLACE-PC.marestdt.com>
2020-02-12 21:49:48 -0300

    Add Babel Plugin & Create TechList Component

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,9 @@ module.exports = {
     path: resolve(__dirname, 'public'),
     filename: 'bundle.js'
   },
+  devServer: {
+    contentBase: resolve(__dirname, 'public')
+  },
   module: {
     rules: [
       {
@@ -13,6 +16,17 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
+        }
+      }, {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' }
+        ]
+      }, {
+        test: /.*\.(gif|png|jpe?g)$/i,
+        use: {
+          loader: 'file-loader'
         }
       }
     ]
